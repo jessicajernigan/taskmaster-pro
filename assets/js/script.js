4ad326c9ef54f55d4102a492e8f3de56f60c6edb
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-06-18 13:12:19 -0500

    custom buttons -- with hover effects -- added. delete dropzone animation complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -160,15 +160,21 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function (event) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
     // console.log("activate", this);
   },
   deactivate: function (event) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
     // console.log("deactivate", this);
   },
   over: function (event) {
+    $(".bottom-trash").addClass("dropover-active");
     // console.log("over", event.target);
   },
   out: function (event) {
+    $(".bottom-trash").removeClass("dropover-active");
     // console.log("out", event.target);
   },
   update: function (event) {
@@ -213,13 +219,16 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function (event, ui) {
     ui.draggable.remove();
-    console.log("drop");
+    $(".bottom-trash").removeClass("bottom-trash-active");
+    // console.log("drop");
   },
   over: function (event, ui) {
-    console.log("over");
+    // console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function (event, ui) {
-    console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
+    // console.log("out");
   }
 });
 
@@ -238,7 +247,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
